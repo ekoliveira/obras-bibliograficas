7426c83449de3449334b40d6faf1e370e06d00ee
Erick Henrique deOliveira <erick.violin@gmail.com>
2020-04-25 03:18:19 -0300

    feat: criacao do projeto ObrasBibliograficas

---
+++ ObrasBibliograficas.Api/src/Api/ObrasBibliograficas/Models/AutorContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ObrasBibliograficas.Models
+{
+    public class AutorContext : DbContext
+    {
+        public AutorContext(DbContextOptions<AutorContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AutorModel> Autor { get; set; }
+    }
+}
---
+++ ObrasBibliograficas.Api/src/Api/ObrasBibliograficas/Models/AutorModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ObrasBibliograficas.Models
+{
+    public class AutorModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Nome { get; set; }
+    }
+}
